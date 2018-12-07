43b36b37a5c769800d2161fdf8c03828cd8f36c3
Samuel Novaes <samuel.novaes.96@gmail.com>
2018-12-07 00:40:13 -0200

    Redirects from labkids-dados

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ const Bundler = require('parcel-bundler')
 const path = require('path')
 const serveStatic = require('serve-static')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 const bundler = new Bundler(path.join(__dirname, 'client', 'index.html'))
 const router = express.Router()
+const gitData = 'http://samuelnovaes.ml/labkids-dados'
 
 app.use('/assets', serveStatic(path.join(__dirname, 'client', 'assets')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/games', (req, res) => {
-	res.redirect('https://cdn.staticaly.com/gh/LouisRiverstone/CriacaoJogosLabKids/master/index.html')
-})
-
 router.use('/galeria', require('./routes/galeria'))
 router.use('/mensagem', require('./routes/mensagem'))
 
 app.use('/api', router)
-app.use(bundler.middleware())
-app.listen(process.env.PORT || 8080, () => {
-	console.log('Rodando!')
+
+axios.get(`${gitData}/redirects.json`).then(response => {
+	response.data.forEach(redirect => {
+		app.get(redirect.route, (req, res) => {
+			res.redirect(redirect.url)
+		})
+	})
+	app.use(bundler.middleware())
+	app.listen(process.env.PORT || 8080, () => {
+		console.log('Rodando!')
+	})
 })
